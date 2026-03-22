4050d6d8fb71d7771fe02cc02eaf74f3174c871f
Sergittko <derov180214@gmail.com>
2026-03-22 23:11:18 +0200

    fix: design issues

--- src/components/layout/TopBar/index.tsx
+++ src/components/layout/TopBar/index.tsx
@@ -8,6 +8,14 @@ import { AppRoutePathEnum } from '@/types/routes';
 const TopBar = () => {
   const pathname = usePathname();
 
+  const isActive = (pathname: string, path: AppRoutePathEnum) => {
+    if (path === AppRoutePathEnum.HOME) {
+      return pathname === path;
+    }
+
+    return pathname.startsWith(path);
+  };
+
   return (
     <header className="fixed top-0 z-50 max-w-full min-w-full px-6">
       <div className="mx-auto mt-4 flex h-16 w-full max-w-6xl items-center justify-between rounded-[40px] border border-white/10 bg-white/10 px-6 shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-xl supports-backdrop-filter:bg-white/10">
@@ -18,13 +26,15 @@ const TopBar = () => {
         </div>
 
         {/* Center */}
-        <nav className="hidden items-center gap-8 text-sm md:flex">
+        <nav className="hidden items-center gap-2 text-sm md:flex">
           {topBarRoutes.map(({ name, path }) => (
             <Link
               key={name + path}
               href={path}
-              className={`transition-colors hover:text-white ${
-                pathname.includes(path) ? 'text-white' : 'text-white/70'
+              className={`rounded-full border border-transparent px-3 py-1 transition-colors hover:border-white/5 hover:text-white ${
+                isActive(pathname, path)
+                  ? 'rounded-full border border-white/10! bg-white/7 font-semibold text-white shadow-[0_0_10px_rgba(255,255,255,0.10)]'
+                  : 'text-white/70'
               }`}
             >
               {name}
